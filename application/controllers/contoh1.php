9020fdaa9f1b49440c9cd3f53c06269feb022324
renitajulia <renitajulia26@gmail.com>
2021-10-09 12:45:30 +0700

    upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Reni tri anuary julia
+        Saya tinggal di daerah Bekasi
+        Olahraga yang saya sukai adalah bulutangkis";
+    }
+}
